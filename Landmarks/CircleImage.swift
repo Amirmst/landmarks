ee9794d226de3523fea74aa58232a2fd3c73ea60
Amir Mostafavi <amireza.mostafavi@yahoo.com>
2019-12-18 15:30:02 +0330

    finished

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by Amir Mostafavi on 12/15/19.
+//  Copyright © 2019 amir. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
---
+++ Landmarks/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Amir Mostafavi on 12/15/19.
+//  Copyright © 2019 amir. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 200)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading){
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarkData[0])
+    }
+}
